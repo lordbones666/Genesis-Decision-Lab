feb680c89c042fdc39e3ee57bf6cb09edb364f1c
lordbones666 <ogddogg04@gmail.com>
2026-03-16 15:49:04 -0400

    Delete src/retrieval directory

--- src/retrieval/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Decision Lab retrieval substrate seed."""
-
-from .index import RetrievalIndex
-from .ingest import IngestedChunk, IngestionConfig, ingest_paths
-
-__all__ = ["IngestionConfig", "IngestedChunk", "RetrievalIndex", "ingest_paths"]
